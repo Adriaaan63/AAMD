1b4fb57beb7ced25a5465b07c4c9a42a01baac9a
Adriaaan63 <adcast07@ucm.es>
2025-09-25 17:34:25 +0200

    Practica 1 regresion lineal

---
+++ Enunciado/LinearRegression.py
@@ -0,0 +1,115 @@
+import numpy as np
+import copy
+import math
+
+class LinearReg:
+    """
+    Computes the cost function for linear regression.
+
+    Args:
+        x (ndarray): Shape (m,) Input to the model
+        y (ndarray): Shape (m,) the real values of the prediction
+        w, b (scalar): Parameters of the model
+    """
+    def __init__(self, x, y,w,b):
+        #(scalar): Parameters of the model
+        self.x = x
+        self.y = y
+        self.w = w 
+        self.b = b
+
+    """
+    Computes the linear regression function.
+
+    Args:
+        x (ndarray): Shape (m,) Input to the model
+    
+    Returns:
+        the linear regression value
+    """
+    def f_w_b(self, x):
+        return self.w * x + self.b
+
+
+    """
+    Computes the cost function for linear regression.
+    J() equivale a MSE
+
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+    """
+    def compute_cost(self):
+        total_cost = 0
+        m = len(self.x)
+        y_pred = self.f_w_b(self.x)
+
+        total_cost = (1 / (2 * m)) * np.sum((self.y - y_pred) ** 2)
+        return np.float64(total_cost)
+    
+
+    """
+    Computes the gradient for linear regression 
+    Args:
+
+    Returns
+      dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
+      dj_db (scalar): The gradient of the cost w.r.t. the parameter b     
+     """
+    def compute_gradient(self):
+        dj_dw = 0
+        dj_db = 0
+        m = len(self.x)
+        y_pred = self.f_w_b( self.x)
+
+        dj_dw = (1 / m) * np.sum((y_pred - self.y) * self.x)
+        dj_db = (1 / m) * np.sum((y_pred - self.y))
+
+        return np.float64(dj_dw), np.float64(dj_db)
+    
+    
+    """
+    Performs batch gradient descent to learn theta. Updates theta by taking 
+    num_iters gradient steps with learning rate alpha
+
+    Args:
+      alpha : (float) Learning rate
+      num_iters : (int) number of iterations to run gradient descent
+    Returns
+      w : (ndarray): Shape (1,) Updated values of parameters of the model after
+          running gradient descent
+      b : (scalar) Updated value of parameter of the model after
+          running gradient descent
+      J_history : (ndarray): Shape (num_iters,) J at each iteration,
+          primarily for graphing later
+      w_initial : (ndarray): Shape (1,) initial w value before running gradient descent
+      b_initial : (scalar) initial b value before running gradient descent
+    """
+    def gradient_descent(self, alpha, num_iters):
+        # An array to store cost J and w's at each iteration — primarily for graphing later
+        J_history = []
+        w_history = []
+        w_initial = copy.deepcopy(self.w)  # avoid modifying global w within function
+        b_initial = copy.deepcopy(self.b)  # avoid modifying global w within function
+
+        for i in range(num_iters):  
+            dj_dw, dj_db = self.compute_gradient()
+            self.w = self.w - alpha * dj_dw
+            self.b= self.b - alpha * dj_db
+
+            J_history [i] = self.compute_cost()
+            w_history[i] = self.w
+
+        
+        return self.w, self.b, J_history, w_initial, b_initial
+
+
+def cost_test_obj(x,y,w_init,b_init):
+    lr = LinearReg(x,y,w_init,b_init)
+    cost = lr.compute_cost()
+    return cost
+
+def compute_gradient_obj(x,y,w_init,b_init):
+    lr = LinearReg(x,y,w_init,b_init)
+    dw,db = lr.compute_gradient()
+    return dw,db
