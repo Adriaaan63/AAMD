2f1a2b34ed868edb099ea6a9a849f87a01413873
Marc0sPM <marcospm0306@gmail.com>
2025-12-01 17:19:14 +0100

    quitado mean y std de standar scaler

--- Practica5Enuncuado/BattleCity/LearningPy/data_mining.py
+++ Practica5Enuncuado/BattleCity/LearningPy/data_mining.py
@@ -47,8 +47,8 @@ std = np.sqrt(scaler.var_)
 if not os.path.exists(EXPORT_DIR):
     os.makedirs(EXPORT_DIR)
 with open(os.path.join(EXPORT_DIR, "StandardScaler.txt"), "w") as f:
-    f.write("MEAN\n" + ",".join(map(str, mean)) + "\n")
-    f.write("STD\n" + ",".join(map(str, std)) + "\n")
+    f.write(",".join(map(str, mean)) + "\n")
+    f.write(",".join(map(str, std)) + "\n")
 print(">> StandardScaler.txt generado.")
 
 # 5. Visualizar (PCA)
